62af64f98d79ef4d714483f865f0a2835a704831
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2020-10-20 21:42:45 -0500

    Setup logging.

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request
 from climate_eyes import ClimateClient
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def getHistoryByYear():
   return client.getHistoryByYear(location, start_year, end_year, units)
 
 if __name__=="__main__":
+  logging.basicConfig(filename='climate_eyes.log', level=logging.DEBUG, format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s')
   app.run(ssl_context=('fullchain.pem', 'privkey.pem'), port=5002)
